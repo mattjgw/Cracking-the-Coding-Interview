e97fe1bf7e2d9a1b780d1b6c68b1d7bb4589be70
mattjgw <mattjgw@gmail.com>
2018-07-08 15:21:59 -0400

    Question 3.1

---
+++ Stacks and Queues/Three in One/src/ThreeInOne.java
@@ -0,0 +1,21 @@
+
+// Describe how you could use a single array to implement three stacks
+
+public class ThreeInOne {
+	
+	// There are a number of ways this could be done. 
+	
+	// One is through fixed division, where you break the array into three 
+	// sections and allow each stack to operate only in their allotted space. 
+	
+	// Another solution is to allow each stack to be flexible in size. You can
+	// do this by having a the stacks shift to accommodate another stack which is 
+	// overflowing. You could make the array circular for further optimization
+
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
